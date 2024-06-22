4baa5818909e826d68c6bd29b084b471e571b8be
shehabAlashoul <alashouls@gmail.com>
2024-06-22 16:18:35 +0300

    test deploy 2

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import reportWebVitals from 'reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <React.StrictMode>
     <App />
+    </React.StrictMode>
 );
-
+reportWebVitals();
